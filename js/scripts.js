5ea32ae0e555288edc415500b91297cacb46222f
Brad Prettyman <brad.prettyman5@gmail.com>
2021-08-27 09:19:01 -0700

    add first prototype for pizza size price

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,16 @@ function Pizza() {
 }
 
 // options we will need for pizza orders -- size, toppings, total price
+
+Pizza.prototype.size = function() {
+  let sizePrice = 0
+
+  if (this.size === 'small') {
+    sizePrice = 8;
+  } else if (this.size === 'medium') {
+    sizePrice = 10;
+  } else if (this.size === 'large') {
+    sizePrice = 12;
+  }
+  this.sizePrice = sizePrice;
+}
